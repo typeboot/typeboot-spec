3de565b92e0310a22bedd70357a08cec40d64187
typeboot <typebootco@gmail.com>
2021-01-06 09:24:52 +1100

    cql: generate alter statements

--- src/main/kotlin/com/typeboot/dataformat/generator/CQLInstructionsGeneratorFactory.kt
+++ src/main/kotlin/com/typeboot/dataformat/generator/CQLInstructionsGeneratorFactory.kt
@@ -18,17 +18,13 @@ class CQLInstructionsGeneratorFactory(private val options: Map<String, String>) 
         println("generate cassandra table ddl")
         val schemaData = schemaName(tableDefinition.subject.schema)
         val table = tableDefinition.subject.table
-        val columns = tableDefinition.fields.flatMap { f ->
-            val columnName = f.name
-            val type = f.type ?: "text"
-            listOf("  $columnName $type")
-        }.joinToString(separator = ",\n")
+        val columns = serialiseColumns(tableDefinition.fields)
 
         val primaryKey = tableDefinition.constraints.filter { c -> c.type.toLowerCase() == "primary key" }.map { c ->
             c.fieldNames.joinToString(prefix = "  PRIMARY KEY(", separator = ", ", postfix = ")")
         }.joinToString("")
 
-        val columnSpec = listOf(columns, primaryKey).filter{ s -> s.isNotEmpty()}.joinToString(separator = ",\n")
+        val columnSpec = listOf(columns, primaryKey).filter { s -> s.isNotEmpty() }.joinToString(separator = ",\n")
 
         val options = tableDefinition.options.orEmpty().map { kv ->
             val value = kv.value.toString().replace("\n", " ").trim()
@@ -41,32 +37,52 @@ class CQLInstructionsGeneratorFactory(private val options: Map<String, String>) 
             }.joinToString(prefix = "CLUSTERING ORDER BY(", separator = ", ", transform = { s -> s }, postfix = ")\n")
         }.joinToString(separator = "")
 
-        val tableOptionsList = listOf(clustering, options).filter{s -> s.trim().isNotEmpty()}
-        val withPrefix = if(tableOptionsList.isNotEmpty()) " WITH " else ""
+        val tableOptionsList = listOf(clustering, options).filter { s -> s.trim().isNotEmpty() }
+        val withPrefix = if (tableOptionsList.isNotEmpty()) " WITH " else ""
         val tableOptions = tableOptionsList.joinToString(prefix = withPrefix, separator = " AND ")
         val instruction = "\ncreate table $schemaData.$table (\n$columnSpec\n)$tableOptions"
 
         return listOf(Instructions(instruction))
     }
 
+    private fun serialiseColumns(fields: List<FieldDefinition>) = fields.flatMap { f ->
+        val type = f.type ?: "text"
+        listOf("  ${f.name} $type")
+    }.joinToString(separator = ",\n")
+
     override fun generateColumn(columnDefinition: ColumnDefinition): List<Instructions> {
         println("generate cassandra column ddl")
-        return listOf()
+        val schema = schemaName(columnDefinition.subject.schema)
+        val table = columnDefinition.subject.table
+        val columnList = serialiseColumns(columnDefinition.fields)
+        val alterStmt = "\nALTER TABLE $schema.$table ADD (\n$columnList\n)"
+        return listOf(Instructions(alterStmt))
     }
 
     override fun generateColumnRemoval(columnRemovalDefinition: ColumnRemovalDefinition): List<Instructions> {
         println("generate cassandra column drop")
-        return listOf()
+        val schema = schemaName(columnRemovalDefinition.subject.schema)
+        val table = columnRemovalDefinition.subject.table
+        val columns = columnRemovalDefinition.fields.map { f -> f.name }.joinToString(separator = ", ")
+        val dropCols = "\nALTER TABLE $schema.$table DROP ($columns)"
+        return listOf(Instructions(dropCols))
     }
 
     override fun generateColumnRename(columnRenameDefinition: ColumnRenameDefinition): List<Instructions> {
         println("generate cassandra column rename")
-        return listOf()
+        val schema = schemaName(columnRenameDefinition.subject.schema)
+        val table = columnRenameDefinition.subject.table
+        return columnRenameDefinition.fields.map { r ->
+            Instructions("\nALTER TABLE $schema.$table RENAME ${r.from} TO ${r.to}")
+        }
     }
 
     override fun generateTableRemoval(tableRemovalDefinition: TableRemovalDefinition): List<Instructions> {
         println("generate cassandra table remove")
-        return listOf()
+        val schema = schemaName(tableRemovalDefinition.subject.schema)
+        val table = tableRemovalDefinition.subject.table
+        val dropStmt = "\nDROP TABLE $schema.$table"
+        return listOf(Instructions(dropStmt))
     }
 
     override fun serialisationProps(): Serialisation {
